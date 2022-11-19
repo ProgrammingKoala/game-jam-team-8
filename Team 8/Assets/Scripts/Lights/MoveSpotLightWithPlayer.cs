7593650ecb9a0abeb7a490122df7b83740a41d0f
Dawid Kazubski <dawid.kazubski@gmail.com>
2022-11-20 00:46:39 +0100

    Improved flashlight

--- Team 8/Assets/Scripts/Lights/MoveSpotLightWithPlayer.cs
+++ Team 8/Assets/Scripts/Lights/MoveSpotLightWithPlayer.cs
@@ -4,12 +4,44 @@ using UnityEngine;
 
 public class MoveSpotLightWithPlayer : MonoBehaviour
 {
+    private bool isFlipped = false;
+    private float previous;
+
     void Update()
     {
         if (GameStatics.playerPosition != null)
-        {
-            transform.position = new Vector3(GameStatics.playerPosition.position.x-1, GameStatics.playerPosition.position.y+0.5f, transform.position.z);
+        {       
+            //Debug.Log(previous);
+            Flip(previous);
+            if (isFlipped)
+            {
+                transform.position = new Vector3(GameStatics.playerPosition.position.x + 1, GameStatics.playerPosition.position.y + 0.5f, transform.position.z);
+            }
+            else
+            {
+                transform.position = new Vector3(GameStatics.playerPosition.position.x - 1, GameStatics.playerPosition.position.y + 0.5f, transform.position.z);
+            }
+            previous = GameStatics.playerPosition.position.x;
         }
 
+    }
+
+    void Flip(float previousX)
+    {
+        
+        if (previousX < GameStatics.playerPosition.position.x && isFlipped)
+        {
+            Vector3 newRotation = new Vector3(0, 76, 0);
+            transform.eulerAngles = newRotation;
+            isFlipped = false;
+            Debug.Log("Fliped 1");
+        }
+        else if (previousX > GameStatics.playerPosition.position.x && !isFlipped)
+        {
+            Vector3 newRotation = new Vector3(0, -76, 0);
+            transform.eulerAngles = newRotation;
+            isFlipped = true;
+            Debug.Log("Fliped 2");
+        }
     }
 }
